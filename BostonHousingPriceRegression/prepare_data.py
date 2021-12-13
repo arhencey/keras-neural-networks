b366fdce132c1da169b0c357339677108e665453
arhencey <arhencey@yahoo.com>
2021-12-13 15:40:43 -0600

    changed how dataset is loaded

--- BostonHousingPriceRegression/prepare_data.py
+++ BostonHousingPriceRegression/prepare_data.py
@@ -3,12 +3,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 # Loads the Boston Housing price regression dataset
 def get_data():
-    train, test = boston_housing.load_data()
-
-    X_train = train[0]
-    y_train = train[1]
-    X_test = test[0]
-    y_test = test[1]
+    (X_train, y_train), (X_test, y_test) = boston_housing.load_data()
 
     # Normalize the features
     scaler = MinMaxScaler()
