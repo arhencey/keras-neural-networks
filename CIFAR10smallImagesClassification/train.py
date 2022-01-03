01673cc55e127485eb7a8702f5000beda2647265
arhencey <arhencey@yahoo.com>
2022-01-03 17:45:51 -0600

    added early stopping

--- CIFAR10smallImagesClassification/train.py
+++ CIFAR10smallImagesClassification/train.py
@@ -1,14 +1,18 @@
 from prepare_data import get_data
 from model import get_model
+from tensorflow.keras.callbacks import EarlyStopping
 
 X_train, y_train, X_test, y_test = get_data()
 model = get_model()
+
+es = EarlyStopping(monitor='val_loss', mode='min', patience=5)
 
 history = model.fit(
     X_train,
     y_train,
     validation_data=(X_test,y_test),
     epochs=250,
+    callbacks=[es]
 )
 
 
